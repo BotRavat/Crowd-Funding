b67a8d4d162e7442891adae50ad70afb95586a63
RavaT <7sravat@gmail.com>
2023-07-10 15:43:36 +0530

    Create campaign UI added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -13,6 +13,9 @@ export default function App() {
         <Navbar />
         <Routes>
           <Route path="/" element={<Home />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/create-campaign" element={<CreateCampaign />} />
+          <Route path="/campaign-details/:id" element={<CampaignDetails />} />
         </Routes>
       </div>
     </div>
